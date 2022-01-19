ab089325f51e0550062b38d1018c013e33f1fb80
elmonstro <jratcher@gmail.com>
2022-01-19 17:40:09 +0300

    remove swagger

--- medzako/urls.py
+++ medzako/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path("api/auth/", include("authentication.urls", namespace="auth")),
     path("api/medications/", include("medication.urls", namespace="medication")),
     path("api/orders/", include("order.urls", namespace="order")),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # url(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
