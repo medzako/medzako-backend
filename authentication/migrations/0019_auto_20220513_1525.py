5d9e8d4ed3141c2aa7120683b503d6bc3e3b4a41
elmonstro <jratcher@gmail.com>
2022-05-13 19:13:15 +0300

    change defaulrider value for current order location

--- authentication/migrations/0019_auto_20220513_1525.py
+++ authentication/migrations/0019_auto_20220513_1525.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='currentriderlocation',
             name='rider',
-            field=models.OneToOneField(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='current_location', to='authentication.user'),
+            field=models.OneToOneField(default=111111111, on_delete=django.db.models.deletion.CASCADE, related_name='current_location', to='authentication.user'),
             preserve_default=False,
         ),
     ]
